3b129ddb52509e34031b8f496e477945f670fb33
Andrew Hyndman <ahyndman@dropbox.com>
2022-03-16 17:39:04 -0700

    Stub a simple test server

---
+++ test/server/server.mjs
@@ -0,0 +1,44 @@
+import path from 'path';
+
+import express from 'express';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const PORT = process.env.PORT ?? 3000;
+
+const app = express();
+
+// disable browser cache
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-cache');
+  next();
+});
+
+// Pass a query parameter to artificially delay request handling
+// e.g. localhost:3000/api/hello?delay=1000
+app.use(({query}, res, next) => {
+  if (query?.delay) {
+    setTimeout(next, query.delay);
+  } else {
+    next();
+  }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/dist', express.static(path.join(__dirname, '../../dist')));
+
+// app.get('/', (req, res) => {
+//   // TODO: show something useful?
+// });
+
+app.get('/api/hello', (req, res) => {
+  res.send('world');
+});
+
+app.get('/test/:view', ({params}, res) => {
+  const view = params.view;
+  res.sendFile(`public/${view}.html`, {root: __dirname});
+});
+
+app.listen(PORT, () => {
+  console.log(`test:server listening on http://localhost:${PORT}`);
+});
