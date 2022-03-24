22675fa6a4334756e43d43fd3462e401b2c83ddb
Andrew Hyndman <ahyndman@dropbox.com>
2022-03-24 16:26:16 -0700

    implement e2e test for happy path

--- test/e2e/ajax-mutation.spec.ts
+++ test/e2e/ajax-mutation.spec.ts
@@ -5,9 +5,8 @@ const PAGELOAD_DELAY = 1000;
 const AJAX_DELAY = 500; // see text-mutation.html
 
 test.describe('TTVC', () => {
-  // TODO(PERF-650): This test does not pass!
-  test.skip('respects delayed text-only mutations', async ({page}) => {
-    await page.goto(`http://localhost:3000/test/text-mutation?delay=${PAGELOAD_DELAY}`, {
+  test('respects a mutation triggered after an AJAX request', async ({page}) => {
+    await page.goto(`http://localhost:3000/test/ajax-mutation?delay=${PAGELOAD_DELAY}`, {
       waitUntil: 'networkidle',
     });
 
