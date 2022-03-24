d98b623c613f66b03ec738c5a17e3e411606a728
Andrew Hyndman <ahyndman@dropbox.com>
2022-03-24 16:26:16 -0700

    implement e2e test for happy path

--- test/server/public/analytics.mjs
+++ test/server/public/analytics.mjs
@@ -1,8 +1,19 @@
-import {getTTVC} from '/dist/index.js';
+import {PageLoadVisuallyCompleteCalculator} from '/dist/index.js';
 
 // use window.entries to communicate between browser and test runner processes
 window.entries = [];
 
+const calculator = new PageLoadVisuallyCompleteCalculator();
+calculator.start();
+
+export async function getTTVC(callback /*: (ms: number) => void*/) {
+  const measurement = await calculator.attemptMeasurement();
+  callback(measurement);
+}
+
 getTTVC((ms) => {
-  window.entries.push(ms);
+  console.log('TTVC:', ms);
+  if (ms != null) {
+    window.entries.push(ms);
+  }
 });
