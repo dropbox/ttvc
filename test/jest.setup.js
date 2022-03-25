9d65d95ab4c9cd72f0efa6f887e37dfd57ba68d4
Andrew Hyndman <ahyndman@dropbox.com>
2022-03-24 18:55:32 -0700

    Install and configure jest to support existing unit tests

---
+++ test/jest.setup.js
@@ -0,0 +1,35 @@
+// TODO: Consider rewriting unit tests so that they do not require these global stubs
+
+// stub the MutationObserver and IntersectionObserver interfaces
+global.MutationObserver = class {
+  constructor(callback) {}
+  disconnect() {}
+  observe(element, initObject) {}
+};
+
+global.IntersectionObserver = class {
+  constructor(callback) {}
+  disconnect() {}
+  observe() {}
+  takeRecords() {}
+  unobserve() {}
+};
+
+// jsdom has no performance.navigation.  This is an attempt to
+// give it something to use instead
+window.performance.navigation = {
+  TYPE_NAVIGATE: 0,
+  TYPE_RELOAD: 1,
+  TYPE_BACK_FORWARD: 2,
+  TYPE_RESERVED: 255,
+  redirectCount: 0,
+  type: 0, // TYPE_NAVIGATE
+};
+
+// some tests need this
+window.performance.timing = {
+  // this is an arbitrary timestamp sometime in Feb 2018. We expect tests
+  // shouldn't care what year / day it is, but we used a fixed one for
+  // repeatability.
+  navigationStart: 1518224417028,
+};
