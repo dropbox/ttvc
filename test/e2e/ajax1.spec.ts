e387a6f1094ac43562ed9fcdd5226cdb95c4f34a
Andrew Hyndman <ahyndman@dropbox.com>
2022-03-28 17:25:30 -0700

    Rename e2e tests with consistent naming scheme

--- test/e2e/ajax1.spec.ts
+++ test/e2e/ajax1.spec.ts
@@ -1,13 +1,13 @@
-import {test, expect, Page} from '@playwright/test';
-import {getEntries, entryCountIs} from '../util/entries';
+import {test, expect} from '@playwright/test';
+import {getEntries} from '../util/entries';
 
 const PAGELOAD_DELAY = 1000;
 const AJAX_DELAY = 500; // see text-mutation.html
 
 test.describe('TTVC', () => {
-  test('respects a mutation triggered after an AJAX request', async ({page, browserName}) => {
+  test('a mutation triggered after an AJAX request', async ({page, browserName}) => {
     test.fail(browserName === 'webkit'); // webkit delays first paint until after all scripts load
-    await page.goto(`http://localhost:3000/test/ajax-mutation?delay=${PAGELOAD_DELAY}`, {
+    await page.goto(`http://localhost:3000/test/ajax1?delay=${PAGELOAD_DELAY}`, {
       waitUntil: 'networkidle',
     });
 
---
+++ test/e2e/ajax2.spec.ts
@@ -0,0 +1,19 @@
+import {test, expect} from '@playwright/test';
+import {getEntries} from '../util/entries';
+
+const PAGELOAD_DELAY = 1000;
+const AJAX_DELAY = 500; // see text-mutation.html
+
+test.describe('TTVC', () => {
+  test('a text-only mutation triggered after AJAX', async ({page}) => {
+    test.fail(); // ttvc should not require mutations to mark a timestamp
+    await page.goto(`http://localhost:3000/test/ajax2?delay=${PAGELOAD_DELAY}`, {
+      waitUntil: 'networkidle',
+    });
+
+    const entries = await getEntries(page);
+
+    expect(entries.length).toBe(1);
+    expect(entries[0]).toBeGreaterThanOrEqual(PAGELOAD_DELAY + AJAX_DELAY);
+  });
+});
