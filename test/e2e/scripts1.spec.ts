1292d5970110452bf693641200e3d3adb99c5ee3
Andrew Hyndman <ahyndman@dropbox.com>
2022-03-28 19:14:57 -0700

    Define e2e tests: scripts

---
+++ test/e2e/scripts1.spec.ts
@@ -0,0 +1,22 @@
+import {test, expect} from '@playwright/test';
+
+import {FUDGE} from '../util/constants';
+import {getEntries} from '../util/entries';
+
+const PAGELOAD_DELAY = 200;
+const SCRIPT_DELAY = 500;
+
+test.describe('TTVC', () => {
+  test('a single loading script tag and then a mutation', async ({page}) => {
+    test.fail(); // FIXME: ttvc should wait for at least an animation frame after last loading script!
+    await page.goto(`http://localhost:3000/test/scripts1?delay=${PAGELOAD_DELAY}`, {
+      waitUntil: 'networkidle',
+    });
+
+    const entries = await getEntries(page);
+
+    expect(entries.length).toBe(1);
+    expect(entries[0]).toBeGreaterThanOrEqual(PAGELOAD_DELAY + SCRIPT_DELAY);
+    expect(entries[0]).toBeLessThanOrEqual(PAGELOAD_DELAY + SCRIPT_DELAY + FUDGE);
+  });
+});
---
+++ test/server/public/mutation.mjs
@@ -0,0 +1,4 @@
+// This script immediately mutates the document after loading.
+const p = document.createElement('p');
+p.innerText = 'This is a paragraph.';
+document.body.appendChild(p);
