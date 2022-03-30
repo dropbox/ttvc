b75076ff2794d7e9203a9fab1a38939f44943cb6
Andrew Hyndman <ahyndman@dropbox.com>
2022-03-30 14:14:40 -0700

    Mark webkit test failure

--- test/e2e/ajax-mutation.spec.ts
+++ test/e2e/ajax-mutation.spec.ts
@@ -5,7 +5,8 @@ const PAGELOAD_DELAY = 1000;
 const AJAX_DELAY = 500; // see text-mutation.html
 
 test.describe('TTVC', () => {
-  test('respects a mutation triggered after an AJAX request', async ({page}) => {
+  test('respects a mutation triggered after an AJAX request', async ({page, browserName}) => {
+    test.fail(browserName === 'webkit'); // webkit delays first paint until after all scripts load
     await page.goto(`http://localhost:3000/test/ajax-mutation?delay=${PAGELOAD_DELAY}`, {
       waitUntil: 'networkidle',
     });
