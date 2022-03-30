87a4d8e4ad91eb5d6043a281c6a8cc959b60b67f
Andrew Hyndman <ahyndman@dropbox.com>
2022-03-29 17:12:46 -0700

    Define e2e tests: react

---
+++ test/e2e/react2.spec.ts
@@ -0,0 +1,22 @@
+import {test, expect} from '@playwright/test';
+
+import {FUDGE} from '../util/constants';
+import {getEntries} from '../util/entries';
+
+const PAGELOAD_DELAY = 200;
+const IMAGE_DELAY = 500;
+
+test.describe('TTVC', () => {
+  test('a simple react application with an image', async ({page}) => {
+    test.fail(); // FIXME: ttvc is initialized too late on this page
+    await page.goto(`http://localhost:3000/test/static1?delay=${PAGELOAD_DELAY}`, {
+      waitUntil: 'networkidle',
+    });
+
+    const entries = await getEntries(page);
+
+    expect(entries.length).toBe(1);
+    expect(entries[0]).toBeGreaterThanOrEqual(PAGELOAD_DELAY + IMAGE_DELAY);
+    expect(entries[0]).toBeLessThanOrEqual(PAGELOAD_DELAY + IMAGE_DELAY + FUDGE);
+  });
+});
