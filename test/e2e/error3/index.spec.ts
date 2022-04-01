cb0cd7d6fe2fa44bd69fd37e8c2f008a327619df
Andrew Hyndman <ahyndman@dropbox.com>
2022-03-31 18:08:47 -0700

    fixup! Define e2e tests: error

--- test/e2e/error3/index.spec.ts
+++ test/e2e/error3/index.spec.ts
@@ -4,7 +4,7 @@ import {FUDGE} from '../../util/constants';
 import {getEntries} from '../../util/entries';
 
 const PAGELOAD_DELAY = 200;
-const IMAGE_DELAY = 500;
+const SCRIPT_DELAY = 200;
 
 test.describe('TTVC', () => {
   test('an appended script that errors (404) + a mutation + background lazyloaded script', async ({
@@ -17,7 +17,7 @@ test.describe('TTVC', () => {
     const entries = await getEntries(page);
 
     expect(entries.length).toBe(1);
-    const expectedTtvc = PAGELOAD_DELAY + IMAGE_DELAY;
+    const expectedTtvc = PAGELOAD_DELAY + SCRIPT_DELAY;
     expect(entries[0]).toBeGreaterThanOrEqual(expectedTtvc);
     expect(entries[0]).toBeLessThanOrEqual(expectedTtvc + FUDGE);
   });
