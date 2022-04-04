2e7c45da4a1759d4fb24acc1e9c7878ba7d23efe
Andrew Hyndman <ahyndman@dropbox.com>
2022-04-04 14:05:36 -0700

    entryCountIs accepts a timeout parameter

--- test/util/entries.ts
+++ test/util/entries.ts
@@ -13,7 +13,11 @@ export const getEntries = (page: Page) => page.evaluate(() => window.entries);
 /**
  * Wait until at least {count} performance entries have been logged.
  */
-export const entryCountIs = async (page: Page, count: number, timeout?: number): Promise<void> => {
+export const entryCountIs = async (
+  page: Page,
+  count: number,
+  timeout: number = 5000
+): Promise<void> => {
   await page.waitForFunction((count) => window.entries.length >= count, count, {
     polling: 500,
     timeout: timeout,
