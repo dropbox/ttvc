0515c94042acffa14d18a80d86a92f992a5eee04
Andrew Hyndman <ahyndman@dropbox.com>
2022-04-05 15:56:20 -0700

    Playwright: Set local default to three workers and one retry

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,9 +24,9 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
