4e2171b19e11474703299465da96f9b2e1b58c43
Andrew Hyndman <ahyndman@dropbox.com>
2022-04-05 18:18:29 -0700

    Resolve typecheck errors

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -5,12 +5,14 @@
 // TODO: Consider rewriting unit tests so that they do not require these global stubs
 
 // stub the MutationObserver and IntersectionObserver interfaces
+// @ts-expect-error This is a jest stub
 global.MutationObserver = class {
   constructor(callback) {}
   disconnect() {}
   observe(element, initObject) {}
 };
 
+// @ts-expect-error This is a jest stub
 global.IntersectionObserver = class {
   constructor(callback) {}
   disconnect() {}
@@ -21,6 +23,7 @@ global.IntersectionObserver = class {
 
 // jsdom has no performance.navigation.  This is an attempt to
 // give it something to use instead
+// @ts-expect-error This is a jest stub
 window.performance.navigation = {
   TYPE_NAVIGATE: 0,
   TYPE_RELOAD: 1,
@@ -31,6 +34,7 @@ window.performance.navigation = {
 };
 
 // some tests need this
+// @ts-expect-error This is a jest stub
 window.performance.timing = {
   // this is an arbitrary timestamp sometime in Feb 2018. We expect tests
   // shouldn't care what year / day it is, but we used a fixed one for
