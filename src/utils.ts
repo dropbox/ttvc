87b192624942d29eb93d9bd185967de3cc5b292b
Andrew Hyndman <ahyndman@dropbox.com>
2022-04-06 16:17:55 -0700

    Delete now-unused util function

--- src/utils.ts
+++ src/utils.ts
@@ -6,10 +6,12 @@ export const waitForPageLoad = () => {
     return Promise.resolve();
   }
 
-  return new Promise<void>((resolve, reject) => {
-    window.addEventListener('load', () => {
+  return new Promise<void>((resolve) => {
+    const handleEvent = () => {
       resolve();
-    });
+      window.removeEventListener('load', handleEvent);
+    };
+    window.addEventListener('load', handleEvent);
   });
 };
 
@@ -25,22 +27,5 @@ export const requestIdleCallback = (callback: () => void) => {
     // wait *two* animation frames, to ensure the page has a chance to flush
     // pending changes to the DOM
     window.requestAnimationFrame(() => window.requestAnimationFrame(callback));
-  });
-};
-
-/**
- * Returns a promise resolved when the page has transitioned to a hidden state.
- */
-export const waitForPageHidden = () => {
-  return new Promise<void>((resolve, reject) => {
-    const onHiddenOrPageHide = (event: Event) => {
-      if (event.type === 'pagehide' || document.visibilityState === 'hidden') {
-        document.removeEventListener('visibilitychange', onHiddenOrPageHide, true);
-        window.removeEventListener('pagehide', onHiddenOrPageHide, true);
-        resolve();
-      }
-    };
-    document.addEventListener('visibilitychange', onHiddenOrPageHide, true);
-    window.addEventListener('pagehide', onHiddenOrPageHide, true);
   });
 };
