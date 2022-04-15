eb7c9140e2c7001b939759ef9c0ce3314817d178
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2022-04-15 12:56:32 -0600

    More playwright tests! (#17)
    * tests
    
    * added a better test for unsuported envirioment
    
    * adressed lint errors
    
    * Lint update
    
    Co-authored-by: jonathan calderon <jcalderon@dropbox.com>

---
+++ test/e2e/error5/index.spec.ts
@@ -0,0 +1,21 @@
+import {test, expect} from '@playwright/test';
+
+const PAGELOAD_DELAY = 200;
+
+const unsupportedErrorMessage = new Error("VisuallyCompleteCalculator: This browser/runtime is not supported.");
+
+test.describe('TTVC', () => {
+  test('an unsupported environment', async ({page}) => {
+    const errorCount = [];
+    page.on('pageerror', exception => {
+      errorCount.push(exception)
+    });
+
+    await page.goto(`/test/error5?delay=${PAGELOAD_DELAY}`, {
+      waitUntil: 'networkidle',
+    });
+
+    expect(errorCount.length).toBe(1);
+    expect(errorCount[0]).toEqual(unsupportedErrorMessage);
+  });
+});
