955e61b641ffd7516a1159b096f0127d50a6b69a
Andrew Hyndman <ahyndman@dropbox.com>
2022-04-15 19:45:04 -0700

    Define test case

---
+++ test/e2e/error6/index.spec.ts
@@ -0,0 +1,25 @@
+import {test, expect} from '@playwright/test';
+
+import {FUDGE} from '../../util/constants';
+import {getEntries} from '../../util/entries';
+
+const PAGELOAD_DELAY = 200;
+
+test.describe('TTVC', () => {
+  test('text that is removed before it can be processed', async ({page}) => {
+    const errors = [];
+    page.on('pageerror', (exception) => errors.push(exception));
+
+    await page.goto(`/test/error6?delay=${PAGELOAD_DELAY}`, {
+      waitUntil: 'networkidle',
+    });
+
+    const entries = await getEntries(page);
+
+    expect(errors.length).toBe(0);
+
+    expect(entries.length).toBe(1);
+    expect(entries[0]).toBeGreaterThanOrEqual(PAGELOAD_DELAY);
+    expect(entries[0]).toBeLessThanOrEqual(PAGELOAD_DELAY + FUDGE);
+  });
+});
