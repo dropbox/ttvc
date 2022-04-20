cb58a2cd9050a4d806fb385fc9225e66ddc67a6a
Andrew Hyndman <ahyndman@dropbox.com>
2022-04-19 19:31:15 -0700

    Update config comments to reflect change

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -8,10 +8,16 @@ export type TtvcOptions = {
 export const CONFIG = {
   /** Decide whether to log debug messages. */
   DEBUG: false,
+
   /** A duration in ms to wait before declaring the page completely idle. */
   IDLE_TIMEOUT: 200,
-  /** A duration in ms to wait before assuming that a single network request
-   * was not instrumented correctly. */
+
+  /**
+   * A duration in ms to wait before assuming that a single network request
+   * was not instrumented correctly.
+   *
+   * If NETWORK_TIMEOUT is set to 0, disable this feature.
+   */
   NETWORK_TIMEOUT: 60000,
 };
 
