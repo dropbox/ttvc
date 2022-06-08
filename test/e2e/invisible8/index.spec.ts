62df3443b7b6bf7bc3ea6738a2f261e0e4b0270f
Andrew Hyndman <ajhyndman@hotmail.com>
2022-06-08 13:53:20 -0700

    PERF-810 — Add test illustrating collapsed sidebar behavior (#47)
    * Add test illustrating collapsed sidebar behavior
    
    * Add comment to test scenario
    
    Co-authored-by: Andrew Hyndman <ahyndman@dropbox.com>

---
+++ test/e2e/invisible8/index.spec.ts
@@ -0,0 +1,22 @@
+import {test, expect} from '@playwright/test';
+
+import {FUDGE} from '../../util/constants';
+import {getEntries} from '../../util/entries';
+
+const PAGELOAD_DELAY = 200;
+const MUTATION_DELAY = 500;
+
+test.describe('TTVC', () => {
+  test('a visible mutation + a mutation with invisible parent', async ({page}) => {
+    await page.goto(`/test/invisible8?delay=${PAGELOAD_DELAY}`, {
+      waitUntil: 'networkidle',
+    });
+
+    const entries = await getEntries(page);
+
+    expect(entries.length).toBe(1);
+    const expectedTtvc = PAGELOAD_DELAY + MUTATION_DELAY;
+    expect(entries[0].duration).toBeGreaterThanOrEqual(expectedTtvc);
+    expect(entries[0].duration).toBeLessThanOrEqual(expectedTtvc + FUDGE);
+  });
+});
