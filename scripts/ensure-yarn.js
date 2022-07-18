cc61bce923fadc15c6ecc49d58deb9c25cefc2cf
Andrew Hyndman <ajhyndman@hotmail.com>
2022-07-18 17:52:23 -0400

    Revert "Disallow npm usage" (#61)
    * Revert "Disallow `npm install` (#41)"
    
    This reverts commit 031db7fe0970d8c0c045172e08c74b1039cc4875.
    
    * Restore .npmrc change
    
    Co-authored-by: Andrew Hyndman <ahyndman@dropbox.com>

--- scripts/ensure-yarn.js
+++
@@ -1,8 +0,0 @@
-const agent = process.env.npm_config_user_agent;
-
-if (!agent?.startsWith('yarn')) {
-  console.error(
-    'Please use yarn to manage dependencies in this repository.\n  $ npm install --global yarn\n'
-  );
-  process.exit(1);
-}
