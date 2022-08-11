bcc21ed5494fc8dc61306162bf23507dff57d3a8
Andrew Hyndman <ahyndman@dropbox.com>
2022-08-11 14:28:18 -0700

    Assert expected diagnostic detail

---
+++ test/e2e/output1/index.spec.ts
@@ -0,0 +1,38 @@
+import {test, expect} from '@playwright/test';
+
+import {FUDGE} from '../../util/constants';
+import {getEntries} from '../../util/entries';
+
+const PAGELOAD_DELAY = 200;
+const IMAGE_DELAY = 500;
+
+test.describe('TTVC', () => {
+  test('output reports timing data', async ({page}) => {
+    await page.goto(`/test/output1?delay=${PAGELOAD_DELAY}`, {
+      waitUntil: 'networkidle',
+    });
+
+    const entries = await getEntries(page);
+
+    expect(entries.length).toBe(1);
+    expect(entries[0].duration).toBeGreaterThanOrEqual(PAGELOAD_DELAY + IMAGE_DELAY);
+    expect(entries[0].duration).toBeLessThanOrEqual(PAGELOAD_DELAY + IMAGE_DELAY + FUDGE);
+    expect(entries[0].start).toBeDefined();
+    expect(entries[0].end).toBeDefined();
+  });
+
+  test('output includes detail', async ({page}) => {
+    await page.goto(`/test/output1?delay=${PAGELOAD_DELAY}`, {
+      waitUntil: 'networkidle',
+    });
+
+    const entries = await getEntries(page);
+
+    expect(entries.length).toBe(1);
+    expect(entries[0].detail).toBeDefined();
+    expect(entries[0].detail.didNetworkTimeOut).toBe(false);
+    expect(entries[0].detail.lastImageLoadTarget).toBeDefined();
+    expect(entries[0].detail.lastImageLoadTimestamp).toBeDefined();
+    expect(entries[0].detail.lastMutation).toBeDefined();
+  });
+});
