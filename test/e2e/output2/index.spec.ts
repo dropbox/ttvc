52f33e46163eb4f6d30e837120a534a7119a1352
Andrew Hyndman <ajhyndman@hotmail.com>
2022-08-16 14:49:02 -0700

    PERF-813 Capture diagnostic details (#63)
    * Assert expected diagnostic detail
    
    * Capture and report diagnostic details
    
    * fix: ?? operator is lower precedence than >= !
    
    * Consolidate diagnostic detail in `lastVisualUpdate` key
    
    * lastVisualUpdate -> lastVisibleChange
    
    * Document new diagnostic detail in README
    
    Co-authored-by: Andrew Hyndman <ahyndman@dropbox.com>

---
+++ test/e2e/output2/index.spec.ts
@@ -0,0 +1,39 @@
+import {test, expect} from '@playwright/test';
+
+import {FUDGE} from '../../util/constants';
+import {getEntries} from '../../util/entries';
+
+const PAGELOAD_DELAY = 200;
+
+test.describe('TTVC', () => {
+  test('output reports timing data', async ({page}) => {
+    await page.goto(`/test/output2?delay=${PAGELOAD_DELAY}`, {
+      waitUntil: 'networkidle',
+    });
+
+    const entries = await getEntries(page);
+
+    expect(entries.length).toBe(1);
+    expect(entries[0].duration).toBeGreaterThanOrEqual(PAGELOAD_DELAY);
+    expect(entries[0].duration).toBeLessThanOrEqual(PAGELOAD_DELAY + FUDGE);
+    expect(entries[0].start).toBeDefined();
+    expect(entries[0].end).toBeDefined();
+  });
+
+  test('output includes detail', async ({page}) => {
+    await page.goto(`/test/output2?delay=${PAGELOAD_DELAY}`, {
+      waitUntil: 'networkidle',
+    });
+
+    const entries = await getEntries(page);
+
+    expect(entries.length).toBe(1);
+    expect(entries[0].detail).toBeDefined();
+    expect(entries[0].detail.didNetworkTimeOut).toBe(false);
+
+    const isMutationRecord = await page.evaluate(() => {
+      return window.entries[0].detail.lastVisibleChange instanceof MutationRecord;
+    });
+    expect(isMutationRecord).toBe(true);
+  });
+});
