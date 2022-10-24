1e4e068403896b2a7373fda52269cd5beb1fbaaa
Andrew Hyndman <ajhyndman@hotmail.com>
2022-10-24 16:58:35 -0700

    Rename getTTVC -> onTTVC (#44)
    Co-authored-by: Andrew Hyndman <ahyndman@dropbox.com>

--- src/visuallyCompleteCalculator.ts
+++ src/visuallyCompleteCalculator.ts
@@ -167,7 +167,7 @@ class VisuallyCompleteCalculator {
   }
 
   /** subscribe to Visually Complete metrics */
-  getTTVC = (subscriber: MetricSubscriber) => {
+  onTTVC = (subscriber: MetricSubscriber) => {
     // register subscriber callback
     this.subscribers.add(subscriber);
 
