203cec76e2d08738223f0654c45d7a2716ef858e
Andrew Hyndman <ahyndman@dropbox.com>
2023-09-06 17:09:35 -0400

    getNetworkIdleObservable should not be called on import

--- src/index.ts
+++ src/index.ts
@@ -101,7 +101,7 @@ export const cancel = (e?: Event) => calculator?.cancel(e);
  * For the most accurate results, `decrementAjaxCount` should be called
  * **exactly once** for each `incrementAjaxCount`.
  */
-export const incrementAjaxCount = getNetworkIdleObservable().incrementAjaxCount;
+export const incrementAjaxCount = () => getNetworkIdleObservable().incrementAjaxCount();
 
 /**
  * Call this to notify ttvc that an AJAX request has just resolved.
@@ -112,4 +112,4 @@ export const incrementAjaxCount = getNetworkIdleObservable().incrementAjaxCount;
  * For the most accurate results, `decrementAjaxCount` should be called
  * **exactly once** for each `incrementAjaxCount`.
  */
-export const decrementAjaxCount = getNetworkIdleObservable().decrementAjaxCount;
+export const decrementAjaxCount = () => getNetworkIdleObservable().decrementAjaxCount();
