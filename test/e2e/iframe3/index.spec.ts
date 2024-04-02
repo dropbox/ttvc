418ac05af73f31899bc26c85d52b48aa4e72c831
Andrew Hyndman <ahyndman@dropbox.com>
2024-04-02 16:39:44 -0400

    Write e2e test case for <iframe src="about:blank">

---
+++ test/e2e/iframe3/index.spec.ts
@@ -0,0 +1,20 @@
+import {test, expect} from '@playwright/test';
+
+import {FUDGE} from '../../util/constants';
+import {getEntries} from '../../util/entries';
+
+const PAGELOAD_DELAY = 200;
+
+test.describe('TTVC', () => {
+  test('an iframe with src="about:blank"', async ({page}) => {
+    await page.goto(`/test/iframe3?delay=${PAGELOAD_DELAY}`, {
+      waitUntil: 'networkidle',
+    });
+
+    const entries = await getEntries(page);
+
+    expect(entries.length).toBe(1);
+    expect(entries[0].duration).toBeGreaterThanOrEqual(PAGELOAD_DELAY);
+    expect(entries[0].duration).toBeLessThanOrEqual(PAGELOAD_DELAY + FUDGE);
+  });
+});
